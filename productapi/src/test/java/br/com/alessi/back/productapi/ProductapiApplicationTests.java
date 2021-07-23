c8e2a17242e77519ac782dc08741730366935b37
ErickBRAlessi <erick_alessi@outlook.com>
2021-07-23 16:37:04 -0300

    no more firefly

---
+++ productapi/src/test/java/br/com/alessi/back/productapi/ProductapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alessi.back.productapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ userapi/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users.user(nome, cpf, endereco, email, telefone, data_cadastro) values ('Inserted from Name', '123', 'Address', 'a@a', '4567-8967', '2020-01-01');
+insert into users.user(nome, cpf, endereco, email, telefone, data_cadastro) values ('Inserted from Name', '123', 'Address', 'a@a', '4567-8967', '2020-01-01');
+insert into users.user(nome, cpf, endereco, email, telefone, data_cadastro) values ('Inserted from Name', '123', 'Address', 'a@a', '4567-8967', '2020-01-01');
